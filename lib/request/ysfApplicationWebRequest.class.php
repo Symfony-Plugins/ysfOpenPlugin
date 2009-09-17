0794f3c3c458cc71b5f4eb01ff5c3aaadf5ece31
dwhittle <dwhittle@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-17 18:37:07 -0500

    ysfOpenPlugin: added ysfApplicationWebRequest that makes it easier to manage yahoo app platform requests
    git-svn-id: http://svn.symfony-project.com/plugins/ysfOpenPlugin/trunk@22111 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/request/ysfApplicationWebRequest.class.php
@@ -0,0 +1,159 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) 2004-2006 Fabien Potencier <fabien.potencier@symfony-project.com>
+ * (c) 2004-2006 Sean Kerr <sean@code-box.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * ysfApplicationWebRequest class.
+ *
+ * This class manages web requests. It parses input from the request and store them as parameters.
+ *
+ * @package    ysfOpenPlugin
+ * @subpackage request
+ * @author     Dustin Whittle <dustin.whittle@symfony-project.com>
+ * @version    SVN: $Id: ysfApplicationWebRequest.class.php 14634 2009-01-12 09:14:26Z dwhittle $
+ */
+class ysfApplicationWebRequest extends sfWebRequest
+{
+
+  /**
+   * Initializes this sfRequest.
+   *
+   * Available options:
+   *
+   *  * formats:           The list of supported format and their associated mime-types
+   *
+   * @param  sfEventDispatcher $dispatcher  An sfEventDispatcher instance
+   * @param  array             $parameters  An associative array of initialization parameters
+   * @param  array             $attributes  An associative array of initialization attributes
+   * @param  array             $options     An associative array of options
+   *
+   * @return bool true, if initialization completes successfully, otherwise false
+   *
+   * @throws <b>sfInitializationException</b> If an error occurs while initializing this sfRequest
+   *
+   * @see sfRequest
+   */
+  public function initialize(sfEventDispatcher $dispatcher, $parameters = array(), $attributes = array(), $options = array())
+  {
+    parent::initialize($dispatcher, $parameters, $attributes, $options);
+  }
+
+  public function isIphone()
+  {
+    return preg_match('#Mobile/.+Safari#i', $this->getHttpHeader('User-Agent'));
+  }
+
+  public function isOAuth()
+  {
+    return $this->hasParameter('oauth_signature');
+  }
+
+  public function getOAuthSignature()
+  {
+    return $this->getParameter('oauth_signature');
+  }
+
+  public function getOAuthSignatureMethod()
+  {
+    return $this->getParameter('oauth_signature_method', 'HMAC-SHA1');
+  }
+
+  public function isFacebook()
+  {
+
+  }
+
+  public function isFacebookAuthenticated()
+  {
+
+  }
+
+  public function isYahoo()
+  {
+    return $this->hasParameter('yap_appid');
+  }
+
+  public function isYahooAuthenticated()
+  {
+    $guid = $this->getParameter('yap_viewer_guid');
+
+    return ($this->isOAuth() && !empty($guid));
+  }
+
+  public function getYahooView()
+  {
+    return $this->getParameter('yap_view');
+  }
+
+  public function getYahooUserId()
+  {
+    return $this->getParameter('yap_viewer_guid');
+  }
+
+  public function getYahooApplicationId()
+  {
+    return $this->getParameter('yap_appid');
+  }
+
+  public function getYahooDropzoneId()
+  {
+    return $this->getParameter('yap_dropzone_id');
+  }
+
+  public function getYahooConsumerKey()
+  {
+    return $this->getParameter('yap_consumer_key');
+  }
+
+  public function getYahooAccessToken()
+  {
+    return $this->getParameter('yap_viewer_access_token');
+  }
+
+  public function getYahooAccessTokenSecret()
+  {
+    return $this->getParameter('yap_viewer_access_token_secret');
+  }
+
+  public function getYahooTimezone()
+  {
+    return $this->getParameter('yap_tz', 'America/Los_Angeles');
+  }
+
+  public function getYahooCulture()
+  {
+    return $this->getParameter('yap_jurisdiction');
+  }
+
+  public function getYahooRequestedAt()
+  {
+    return $this->getParameter('yap_time');
+  }
+
+  public function getYahooDropzoneDoneUrl($default)
+  {
+    if($this->isYahoo())
+    {
+      /**
+       * Move settings from app.yml to factories.yml options
+       */
+      $dropzones = sfConfig::get('app_yahoo_dropzones');
+
+      $doneUrl = isset($dropzones[$this->getYahooDropzoneId()]) ? str_replace('%s', sfConfig::get('app_yahoo_application_id'), $dropzones[$this->getYahooDropzoneId()]) : $default;
+    }
+    else
+    {
+      $doneUrl = $default;
+    }
+
+    return $doneUrl;
+  }
+
+}
